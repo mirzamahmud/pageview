5147c5d92f32c62327fe5bde016ed34a5b8b6159
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-06-07 23:51:52 +0600

    refactor(pageview): scroll_both_tabbar_and_view

--- lib/controller/home_screen_controller.dart
+++ lib/controller/home_screen_controller.dart
@@ -5,13 +5,33 @@ class HomeScreenController extends GetxController{
 
   HomeScreenController();
 
-  List<String> tabList = ["Overview", "FAQ", "Recent View"];
-  List<Color> colorList = [Colors.green, Colors.red, Colors.blue];
+  List<String> tabList = [
+    "AC Servicing",
+    "AC Cooling Problem",
+    "AC Installation",
+    "AC Hanging",
+    "LCD/LED/Smart TV Repair",
+    "Microwave Repair Service",
+    "Refrigerator Repair",
+    "Washing Machine Repair",
+    "Ambulance Service",
+    "Laptop & Desktop Repair"
+  ];
+  List<Color> colorList = [
+    Colors.green,
+    Colors.red,
+    Colors.blue,
+    Colors.yellow,
+    Colors.orange,
+    Colors.amber,
+    Colors.pink,
+    Colors.black,
+    Colors.grey,
+  ];
   int selectedIndex = 0;
-
   void changeSelectedIndex(int index){
-
     selectedIndex = index;
+    print("Selected Index: $selectedIndex");
     update();
   }
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pageview/controller/home_screen_controller.dart';
-import 'package:pageview/screen/widget/home_tab_view.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -26,50 +25,86 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return GetBuilder<HomeScreenController>(
         builder: (controller) => SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.grey.shade200,
-          appBar: AppBar(
-            elevation: 0,
-            backgroundColor: Colors.white,
-            title: const Text("Pageview Vertical Scroll", style: TextStyle(
-              color: Colors.black,
-              fontSize: 16
-            )),
-            titleSpacing: 0,
-            leading: InkWell(
-              onTap: (){},
-              child: const Icon(Icons.menu, color: Colors.black, size: 16),
-            ),
-            actions: [
-              Padding(
-                padding: const EdgeInsets.only(right: 15),
-                child: InkWell(
-                  onTap: (){},
-                  child: const Icon(Icons.shop, color: Colors.black, size: 16),
+        child: DefaultTabController(
+          length: controller.tabList.length,
+          child: Scaffold(
+            backgroundColor: Colors.grey.shade200,
+            appBar: AppBar(
+              elevation: 0,
+              backgroundColor: Colors.white,
+              title: const Text("Pageview Vertical Scroll", style: TextStyle(
+                color: Colors.black,
+                fontSize: 16
+              )),
+              titleSpacing: 0,
+              leading: InkWell(
+                onTap: (){},
+                child: const Icon(Icons.menu, color: Colors.black, size: 16),
+              ),
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 15),
+                  child: InkWell(
+                    onTap: (){},
+                    child: const Icon(Icons.shop, color: Colors.black, size: 16),
+                  ),
+                ),
+              ],
+              bottom: PreferredSize(
+                preferredSize: Size(MediaQuery.of(context).size.width, 50),
+                child: TabBar(
+                  isScrollable: true,
+                  tabs: List.generate(controller.tabList.length, (index) => Tab(
+                    child: Text(
+                        controller.tabList[index],
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(color: Colors.black)
+                    ),
+                  )),
+                  onTap: (value){
+                    controller.changeSelectedIndex(value);
+                  },
                 ),
               ),
-            ],
-            bottom: PreferredSize(
-              preferredSize: Size(MediaQuery.of(context).size.width, 50),
-              child: const HomeTabView(),
             ),
-          ),
-          body: PageView.builder(
-            controller: pageController,
-            scrollDirection: Axis.vertical,
-            physics: const BouncingScrollPhysics(),
-            itemCount: controller.colorList.length,
-            itemBuilder: (context, index) => Container(
-              height: 300,
-              width: MediaQuery.of(context).size.width,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: controller.colorList[index]
+            body: RotatedBox(
+              quarterTurns: 1,
+              child: TabBarView(
+                children: List.generate(controller.colorList.length, (index) => Container(
+                  height: 300,
+                  width: MediaQuery.of(context).size.width,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: controller.colorList[index]
+                  ),
+                  child: Text(
+                    controller.tabList[index],
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(color: Colors.white)
+                  ),
+                )),
               ),
-            ),
-            onPageChanged: (index) {
-              controller.changeSelectedIndex(index);
-            },
+            )
+
+
+
+
+            /*PageView.builder(
+              scrollDirection: Axis.vertical,
+              physics: const ScrollPhysics(),
+              itemCount: controller.colorList.length,
+              itemBuilder: (context, index) => Container(
+                height: 300,
+                width: MediaQuery.of(context).size.width,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: controller.colorList[index]
+                ),
+              ),
+              onPageChanged: (value) {
+                controller.changeSelectedIndex(value);
+              },
+            )*/
           ),
         ),
       )
